eb2413cb0b397d46e18160f877330bf811857a9e
Brandt322 <bgamboaa98@gmail.com>
2023-07-24 20:15:26 -0500

    :bug: Solution a loop with json response for http request

--- src/main/java/com/example/demoCrud/model/Book.java
+++ src/main/java/com/example/demoCrud/model/Book.java
@@ -1,5 +1,6 @@
 package com.example.demoCrud.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,12 +10,16 @@ public class Book {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
+
     @Column
     private String title;
+
+    @JsonIgnore // Evita la serialización de la propiedad author al convertir en JSON
     // Establecer la relación Many-to-One con la clase Author
     @ManyToOne
     @JoinColumn(name = "author_id") // Nombre de la columna que contendrá el ID del autor en la tabla "Book"
     private Author author;
+
     @Column
     private String isbn;
     @Column
